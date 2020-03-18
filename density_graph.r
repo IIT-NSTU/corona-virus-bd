c20586968ebe5f0b0abc5ca4bd17ca77f85969d3
Dipok Chandra Das <code.dipok@gmail.com>
2020-03-18 21:11:33 +0600

    Add files via upload

---
+++ density_graph.r
@@ -0,0 +1,11 @@
+  population<-read.csv('population_density_bd.csv')
+  for (d in levels(population$division)) {
+    png(filename = paste('Population_Density_Plot_bd_',d,'.png',sep =''))
+    df<-subset(population,population$division==d)
+    plot(df$area.km2,df$population_thousands,main = paste('Population vs Area of Districts of ',d ,' Bangladesh',sep = ''), xlab = 'Area(sq. KM) ',
+         ylab = 'Population(Thousands) ',
+         col= ifelse(df$population_thousands/df$area.km2 >= 1.0, "red", ifelse(df$population_thousands/df$area.km2 <= 1.0,"green", "black")), pch = 19, cex = 2, lty = "solid", lwd = 2)
+    text(df$area.km2,df$population_thousands, labels=df$district, cex= 0.7)
+    abline(a=0,b=1)
+    dev.off()
+  }
