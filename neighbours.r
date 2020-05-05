0b1ed05274e9663bf78fc74e360003e1932c219e
dipok <code.dipok@gmail.com>
2020-05-05 17:48:47 +0600

    first release

--- neighbours.r
+++ neighbours.r
@@ -1,20 +1,22 @@
-population<-read.csv(file='may_04_n_zero.csv')
+population<-read.csv(file='population_density_bd.csv')
 districts<-population$district
 d_no<-population$D_No
 neighbours_zero<-population$N_ZERO
 may_04_cases<-population$May_04
-
-for (d in levels(population$division)) {
-#  png(filename = paste('may_04_n_zero_plot_div_',d,'.png',sep =''))
-  df<-subset(population,population$division==d)
-  max_x<-max(df$May_04)
-  max_y<-max(df$neighbours_zero_may_04_cases)
-  neighbour_to_own_ratio<-df$neighbours_zero_may_04_cases/df$May_04
-  plot(df$May_04,df$neighbours_zero_may_04_cases,main = paste('COVID-19 Total Cases of Districts vs Neighbours of ',d ,' Division',sep = ''),sub ='May-04-2020', xlab = 'Total Cases of Own',
-       ylab = 'Total Cases of Neighbours(N=0)',
-       col= 'red',xlim = c(-5,max_x+10),ylim=c(-5,max_y+10),
-       pch = 10, cex = 1, lty = "solid", lwd = 2)
-  #polygon(a_x,a_y,col = 'green')
-  text(df$May_04+1,df$neighbours_zero_may_04_cases, labels=paste(df$district,df$May_04,df$neighbours_zero_may_04_cases,sep = ','), cex= 0.7)
-  dev.off()
+neighbours_zero_may_04_cases<-c()
+for (d in d_no) {
+  print(districts[d])
+  print(neighbours_zero[d])
+  ns <- (stringr::str_split(neighbours_zero[d],'-'))
+  ns_score <-0
+  for (n in ns[[1]]) {
+    if(n!='0'&&n!='65'&&n!='66'){
+      d_n <- as.integer(n)
+      ns_score<-ns_score + may_04_cases[d_n]
+    }
+  }
+  print(ns_score)
+  neighbours_zero_may_04_cases<-c(neighbours_zero_may_04_cases,ns_score)
 }
+population<-cbind(population,neighbours_zero_may_04_cases)
+write.csv(population,file = 'may_04_n_zero.csv')
